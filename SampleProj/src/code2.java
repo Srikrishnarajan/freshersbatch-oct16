082f0de0f29a6234260eec6c3545ffaa9273a580
Srikrishnarajan Kumar <srikrishnarajan.skr@gmail.com>
2021-07-30 23:30:39 +0530

    Added code1.java and code2.java files

---
+++ SampleProj/src/code2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class code2 {
+
+	public static void main(String[] args) {
+		
+		int n;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		
+		System.out.println("n = "+n);
+		
+		sc.close();
+	}
+}
